72e4c806dc6eb275df4f9ae727e1ec30ba8b9855
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2020-12-10 00:20:11 +0100

    day 9

---
+++ tests/Day9Test.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace prgTW\AdventOfCode2020\Tests;
+
+use PHPUnit\Framework\TestCase;
+use prgTW\AdventOfCode2020\Impl\Day9;
+
+class Day9Test extends TestCase
+{
+	public function testPart1TestInput(): void
+	{
+		$class = new Day9(__DIR__.'/fixtures/day9/day9_1.txt', 5);
+		self::assertSame(127, $class->part1());
+	}
+
+	public function testPart1RealInput(): void
+	{
+		$class = new Day9(__DIR__.'/../input/day9.txt', 25);
+		self::assertSame(69316178, $class->part1());
+	}
+
+	public function testPart2TestInput(): void
+	{
+		$class = new Day9(__DIR__.'/fixtures/day9/day9_1.txt', 5);
+		self::assertSame(62, $class->part2());
+	}
+
+	public function testPart2RealInput(): void
+	{
+		$class = new Day9(__DIR__.'/../input/day9.txt', 25);
+		self::assertSame(9351526, $class->part2());
+	}
+}
