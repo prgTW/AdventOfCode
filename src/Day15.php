4bb11ab44136197e121e5cf3ad753f138f6962eb
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2020-12-16 00:13:13 +0100

    day 15

---
+++ src/Day15.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace prgTW\AdventOfCode2020\Impl;
+
+use prgTW\AdventOfCode2020\Impl\Contracts\AbstractDayChallenge;
+
+class Day15 extends AbstractDayChallenge
+{
+	/** @var array<string> */
+	private array $startingNumbers;
+
+	/** @noinspection PhpMissingParentConstructorInspection */
+	public function __construct(string $input)
+	{
+		$this->startingNumbers = array_map(static fn(string $number) => (int)$number, explode(',', $input));
+	}
+
+	public function part1(int $iteration): int
+	{
+		$last = [];
+		foreach ($this->startingNumbers as $turn => $startingNumber) {
+			$last[$startingNumber] = $turn;
+		}
+
+		$currentNumber = end($this->startingNumbers);
+		for ($turn = count($this->startingNumbers) - 1; $turn < $iteration; ++$turn) {
+			$previousNumber = $currentNumber;
+			if (!isset($last[$currentNumber])) {
+				$currentNumber = 0;
+			} else {
+				$currentNumber = $turn - $last[$currentNumber];
+			}
+			$last[$previousNumber] = $turn;
+		}
+
+		return $previousNumber;
+	}
+}
